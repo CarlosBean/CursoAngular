a8e1aac528352341925997a6b3d75580fe56a6ed
Carlos Andres Benavides <cabenavidesd@misena.edu.co>
2018-05-03 00:53:17 -0500

    chore: Se añade promesas

---
+++ Seccion2_typescript/Promesas/app.ts
@@ -0,0 +1,23 @@
+let prom1 = new Promise( function( resolve, reject ){
+    setTimeout(() => {
+        console.log("Promesa terminada");
+
+        // Termina bien
+        resolve();
+
+        // Termina mal
+        //reject(); 
+    }, 1500)
+})
+
+// Al ser la promesa un metodo asincrono, mientras obtiene la respuesta
+// el sistema pasa a ejecutar las sgtes ordenes
+console.log("Paso 1"); 
+
+prom1.then( function() {
+    console.log("Ejecutar cuando termine bien!");
+}, function() {
+    console.log("Ejecutar cuando salte todo mal");
+})
+
+console.log("Paso 2");
